3f1f19345f2a6fb96424963a0bf59e1d85c3f940
b16linra <37793838+b16linra@users.noreply.github.com>
2019-03-21 12:22:53 +0100

    Update database.php

--- App/CodeIgniter/application/config/database.php
+++ App/CodeIgniter/application/config/database.php
@@ -74,12 +74,12 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	=> '',
+	'dsn'	=> 'mysql:host=localhost;port=3306;dbname=ccdb;user=root;password=password;charset=utf8;',
 	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
-	'dbdriver' => 'mysqli',
+	'username' => 'root',
+	'password' => 'password',
+	'database' => 'ccdn',
+	'dbdriver' => 'pdo',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
