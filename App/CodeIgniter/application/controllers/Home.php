79ae7dce2c51e3ad776a04af6ba4b4033830f275
b16linra <37793838+b16linra@users.noreply.github.com>
2019-03-21 15:47:33 +0100

    Add skeleton code for homepage (CodeIgniter)
    Add model, view and controller for homepage and update routes.php accordingly.

---
+++ App/CodeIgniter/application/controllers/Home.php
@@ -0,0 +1,15 @@
+<?php
+class Home extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('home_model');
+	}
+
+	public function index()
+	{
+		$this->load->view('home/index');
+	}
+
+}
