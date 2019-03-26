61cb01b501346b81e8da114a6f04bf588e92db50
b16linra <37793838+b16linra@users.noreply.github.com>
2019-03-26 12:14:44 +0100

    Add folder for CSS files and update header.php
    CodeIgniter: Add folder for CSS files and update header.php to use styles.css.

--- App/CodeIgniter/application/views/templates/header.php
+++ App/CodeIgniter/application/views/templates/header.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>css/styles.css">
 	<title><?php echo $title; ?></title>
 </head>
 <body>
