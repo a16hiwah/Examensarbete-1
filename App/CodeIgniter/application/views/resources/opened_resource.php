6f51f078571a88253e8fb4f06d09a42aa718d44a
b16linra <37793838+b16linra@users.noreply.github.com>
2019-04-08 16:03:54 +0200

    Update design and layout for opened resources
    CodeIgniter: Update the design and layout on the page for an opened resource.

--- App/CodeIgniter/application/views/resources/opened_resource.php
+++ App/CodeIgniter/application/views/resources/opened_resource.php
@@ -3,21 +3,23 @@
 		<?php foreach ($resource->result() as $row) : ?>
 		<h1><?php echo $row->title; ?></h1>
 		<div>
+			<div id="opened-resrc-description">Description</div>
 			<p><?php echo $row->description; ?></p>
 		</div>
 		<div>
+			<div id="opened-resrc-body">Content</div>
 			<p><?php echo nl2br($row->body); ?></p>
 		</div>
 		<?php $resource_id = $row->id; ?>
 		<?php endforeach; ?>
 	</div>
 	<div class="comment-section">
-		<h3>Comment section</h3>
+		<h3 id="comment-section-header">Comment section</h3>
 		<!-- Only signed in users can create comments -->
 		<?php if($show_create_comment) : ?>
 			<?php echo form_open(uri_string(), 'id=form-create-comment'); ?>
 			<div class="form-group">
-				<span class="form-label">Create comment</span>
+				<span id="create-comment-lbl" class="form-label">Create comment</span>
 				<?php echo form_textarea($form_comment, set_value('form-comment')); ?>
 				<?php echo form_error('form-comment'); ?>
 			</div>
