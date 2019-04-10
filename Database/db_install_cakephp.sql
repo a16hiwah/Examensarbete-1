a1f3981e2ba854b276e9bd91f0a72a230b0f2dc7
b16linra <37793838+b16linra@users.noreply.github.com>
2019-04-10 10:37:35 +0200

    Add database-install script for CakePHP

---
+++ Database/db_install_cakephp.sql
@@ -0,0 +1,71 @@
+#DROP DATABASE IF EXISTS cakephp;
+
+CREATE DATABASE IF NOT EXISTS cakephp;
+USE cakephp;
+
+CREATE TABLE users (
+	id INT AUTO_INCREMENT,
+    username VARCHAR(64) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    image INT,
+    biography VARCHAR(255),
+    created DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    PRIMARY KEY (id),
+	UNIQUE KEY (username)
+) CHARSET=utf8mb4;
+
+CREATE TABLE profile_images (
+    id INT AUTO_INCREMENT,
+    img_name VARCHAR(255) NOT NULL,
+    img_src varchar(400) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO profile_images (img_name, img_src) VALUES ('Solid green', 'http://localhost/Examensarbete/App/CodeIgniter/images/profile_images/solid_green.png');
+INSERT INTO profile_images (img_name, img_src) VALUES ('Solid blue', 'http://localhost/Examensarbete/App/CodeIgniter/images/profile_images/solid_blue.png');
+
+CREATE TABLE resources (
+    id INT AUTO_INCREMENT,
+    user_id INT,
+    title VARCHAR(255) NOT NULL,
+    slug VARCHAR(191) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    body TEXT NOT NULL,
+    num_of_comments INT NOT NULL,
+    created DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    PRIMARY KEY (id, user_id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    UNIQUE KEY (slug)
+) CHARSET=utf8mb4;
+
+CREATE TABLE collections (
+    id INT AUTO_INCREMENT,
+    user_id INT,
+    title VARCHAR(255) NOT NULL,
+    slug VARCHAR(191) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    body TEXT,
+    created DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    PRIMARY KEY (id, user_id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    UNIQUE KEY (slug)
+) CHARSET=utf8mb4;
+
+CREATE TABLE collections_resources (
+    collection_id INT,
+    resource_id INT,
+    PRIMARY KEY (collection_id, resource_id),
+    FOREIGN KEY (collection_id) REFERENCES collections(id),
+    FOREIGN KEY (resource_id) REFERENCES resources(id)
+) CHARSET=utf8mb4;
+
+CREATE TABLE comments (
+    id INT AUTO_INCREMENT,
+    user_id INT,
+    resource_id INT,
+    body VARCHAR(2000) NOT NULL,
+    created DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    PRIMARY KEY (id, user_id, resource_id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (resource_id) REFERENCES resources(id)
+) CHARSET=utf8mb4;
